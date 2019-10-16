8136e7ad62f379dfce0e078a907503743a89030c
hit2hat <hit2hat@icloud.com>
2019-10-16 21:24:31 +0300

    Updates

---
+++ precache-manifest.72bb052bcb9eaeddd7890308798f8772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ac0437bc9c5116c986479e7c18a45d",
+    "url": "/short-links-vkapps/index.html"
+  },
+  {
+    "revision": "7232f92ce73c2a6f83a2",
+    "url": "/short-links-vkapps/static/css/2.70f3b80d.chunk.css"
+  },
+  {
+    "revision": "7232f92ce73c2a6f83a2",
+    "url": "/short-links-vkapps/static/js/2.9d2891aa.chunk.js"
+  },
+  {
+    "revision": "30a6bff27554384bc379",
+    "url": "/short-links-vkapps/static/js/main.a3b48869.chunk.js"
+  },
+  {
+    "revision": "016042071b3047a63747",
+    "url": "/short-links-vkapps/static/js/runtime-main.1943f01e.js"
+  }
+]);
