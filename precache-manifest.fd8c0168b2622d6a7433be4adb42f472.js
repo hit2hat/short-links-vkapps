a86caa4060907b8d0d5bb9448c728526d8aa08fb
hit2hat <hit2hat@icloud.com>
2019-10-31 10:50:43 +0300

    Updates

--- precache-manifest.fd8c0168b2622d6a7433be4adb42f472.js
+++ precache-manifest.fd8c0168b2622d6a7433be4adb42f472.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4440321080b45d3430fac63c396a3756",
+    "revision": "88a0996497655800e9b69479f862191d",
     "url": "/short-links-vkapps/index.html"
   },
   {
-    "revision": "1cb2d60f55299ddf6d4b",
+    "revision": "256b69330b8aad5d784c",
     "url": "/short-links-vkapps/static/css/2.7214840c.chunk.css"
   },
   {
-    "revision": "275ed237765f0984af6f",
+    "revision": "3a320a0db4b081f07a14",
     "url": "/short-links-vkapps/static/css/main.01440d6c.chunk.css"
   },
   {
-    "revision": "1cb2d60f55299ddf6d4b",
-    "url": "/short-links-vkapps/static/js/2.f59952fe.chunk.js"
+    "revision": "256b69330b8aad5d784c",
+    "url": "/short-links-vkapps/static/js/2.cff6c81d.chunk.js"
   },
   {
-    "revision": "275ed237765f0984af6f",
-    "url": "/short-links-vkapps/static/js/main.27d23a2a.chunk.js"
+    "revision": "3a320a0db4b081f07a14",
+    "url": "/short-links-vkapps/static/js/main.064216bd.chunk.js"
   },
   {
     "revision": "016042071b3047a63747",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/short-links-vkapps/precache-manifest.c83119ff2f9f3c6bc0c93e2b573959d7.js"
+  "/short-links-vkapps/precache-manifest.fd8c0168b2622d6a7433be4adb42f472.js"
 );
 
 self.addEventListener('message', (event) => {
